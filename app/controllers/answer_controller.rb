7a6adaa1527c552d9cdaf954cdd023c5ca4c9a07
github username <github email address>
2017-09-11 13:19:39 -0400

    added class to app_controller

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,1 +1,17 @@
-#1
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    if !logged_in?
+      erb :index, :layout => :'not_logged_in_layout' #=> Log In Page
+    else
+      redirect_to_home_page
+    end
+  end
+end
